bcab02f84e694271a482681432a6aee09bd678d0
KANHU <KANHU@KANHU-PC>
2019-02-13 15:59:06 +0530

    push

---
+++ SimpleProject/src/main/java/Com/RanfordBank/SimpleProject/App.java
@@ -0,0 +1,13 @@
+package Com.RanfordBank.SimpleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SimpleProject/src/test/java/Com/RanfordBank/SimpleProject/Demo.java
@@ -0,0 +1,11 @@
+package Com.RanfordBank.SimpleProject;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello...UiPath");
+	}
+
+}
